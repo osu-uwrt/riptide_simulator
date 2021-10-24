1d0e25e33ea1ee17df3b80545ece52f3a3469e6e
Seth <aldridge.94@osu.edu>
2021-10-24 15:55:05 -0400

    Moving vehicles

--- Assets/Scripts/RosSubscriberExample.cs
+++ Assets/Scripts/RosSubscriberExample.cs
@@ -1,34 +1,49 @@
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
-using PuddlePos = RosMessageTypes.UnityRoboticsDemo.Vector3Msg;
-using PuddlesImuData = RosMessageTypes.UnityRoboticsDemo.ImuMsg;
-using PuddlesThrustData = RosMessageTypes.UnityRoboticsDemo.Float32MultiArrayMsg;
+// using PuddlePos = RosMessageTypes.UnityRoboticsDemo.Vector3Msg;
+// using PuddlesImuData = RosMessageTypes.UnityRoboticsDemo.ImuMsg;
+// using PuddlesThrustData = RosMessageTypes.UnityRoboticsDemo.Float32MultiArrayMsg;
+using GazeboModel = RosMessageTypes.UnityRoboticsDemo.ModelStatesMsg;
+using GazeboPose = RosMessageTypes.Geometry.PoseMsg;
+using GazeboQuaternion = RosMessageTypes.Geometry.QuaternionMsg;
 
 public class RosSubscriberExample : MonoBehaviour
 {
     public GameObject vehicle;
-
+    
     void Start()
     {
-        ROSConnection.GetOrCreateInstance().Subscribe<PuddlePos>("puddles/position", PositionChange);
+        // ROSConnection.GetOrCreateInstance().Subscribe<PuddlePos>("puddles/position", PositionChange);
         // ROSConnection.GetOrCreateInstance().Subscribe<PuddlesImuData>("puddles/imu/data", ImuDataChange);
-        ROSConnection.GetOrCreateInstance().Subscribe<PuddlesThrustData>("puddles/thruster_forces", ThrustChange);
-        
-
+        // ROSConnection.GetOrCreateInstance().Subscribe<PuddlesThrustData>("puddles/thruster_forces", ThrustChange);
+        ROSConnection.GetOrCreateInstance().Subscribe<GazeboModel>("gazebo/model_states", ModelChange);
     }
 
-    void PositionChange(PuddlePos PositionMessage)
-    {
-        Debug.Log(PositionMessage);
-    }
-    
-    // void ImuDataChange(PuddlesImuData ImuDataMessage)
+    // void PositionChange(PuddlePos positionMessage)
     // {
-    //     Debug.Log(ImuDataMessage);
+    //     Debug.Log(positionMessage);
     // }
     
-    void ThrustChange(PuddlesThrustData ThrustDataMessage)
+    // void ImuDataChange(PuddlesImuData imuDataMessage)
+    // {
+    //     Debug.Log(imuDataMessage);
+    // }
+    
+    // void ThrustChange(PuddlesThrustData thrustDataMessage)
+    // {
+        
+    // }
+
+    void ModelChange(GazeboModel modelMessage)
     {
-        Debug.Log(ThrustDataMessage);
+        for(int i = 0; i <= modelMessage.name.Length-1; i++) {
+            if (modelMessage.name[i].Equals("puddles")) {
+                GazeboPose pose = modelMessage.pose[i];
+               // GazeboQuaternion quaternion = pose.orientation;
+                Vector3 rectifiedPos = new Vector3((float)pose.position.x, (float)pose.position.z, (float)pose.position.y); // Translation between reference frames
+              //  Vector3 euler = new Vector3((float)pose.orientation.x, (float)pose.orientation.z, (float)pose.orientation.y);
+                vehicle.transform.position = rectifiedPos;
+            }
+        }
     }
 }
