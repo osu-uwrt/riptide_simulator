71466a0f1cfaf0e8788dfc384878df6828db09ce
coalman321 <cjtucker321@gmail.com>
2022-02-23 12:25:17 -0500

    robot forces gets a list of thruster components on the robot

--- Assets/Scripts/Physics/RobotForces.cs
+++ Assets/Scripts/Physics/RobotForces.cs
@@ -18,15 +18,7 @@ namespace physics
             vehicle = gameObject.GetComponent<Rigidbody>();
 
             // grab all of the thrusters on the vehicle to get their positions and force capabilities
-            Component[] components = underWaterObj.GetComponents(typeof(Component));
-            foreach (Component component in components)
-            {
-                string name = component.toString();
-                if (name.ToLower().Contains("thruster"))
-                {
-                    thrusters.Add(gameObject.GetComponent<Thruster>(name));
-                }
-            }
+            thrusters = new List<Thruster>(FindObjectsOfType<Thruster>());
 
         }
 
