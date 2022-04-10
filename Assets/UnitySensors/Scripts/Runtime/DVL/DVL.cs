58f6123f3a929cf5115e1f472d7d5fedb9f22574
Toby McGuire <tobymcguire461@gmail.com>
2022-04-10 15:46:08 -0400

    DVL

--- Assets/UnitySensors/Scripts/Runtime/DVL/DVL.cs
+++ Assets/UnitySensors/Scripts/Runtime/DVL/DVL.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -8,7 +6,7 @@ using UnityEditor;
 namespace FRJ.Sensor
 {
     [RequireComponent(typeof(Rigidbody))]
-    public class IMU : MonoBehaviour
+    public class DVL : MonoBehaviour
     {
         private Rigidbody _rb;
         private Transform _trans;
@@ -18,79 +16,68 @@ namespace FRJ.Sensor
 
         private Vector4 _geometryQuaternion;
         private Vector3 _angularVelocity;
-        private Vector3 _linearAcceleration;
+        private Vector3 _linearVelocity;
 
         private Noise.Gaussian gaussianNoise;
         private Noise.Bias biasNoise;
 
         [SerializeField] private float _scanRate = 100f;
         public float scanRate { get => this._scanRate; }
-        
+
         public bool enableGaussianNoise;
         public bool enableBiasNoise;
         public NoiseSetting setting = new NoiseSetting();
 
         public Vector4 GeometryQuaternion { get => _geometryQuaternion; }
         public Vector3 AngularVelocity { get => _angularVelocity; }
-        public Vector3 LinearAcceleration { get => _linearAcceleration; }
+        public Vector3 LinearVelocity { get => _linearVelocity; }
 
         [System.Serializable]
         public class NoiseSetting
         {
-            public Vector4 quatSigma;
-            public Vector4 quatBias;
             public Vector3 angVelSigma;
             public Vector3 angVelBias;
-            public Vector3 linAccSigma;
-            public Vector3 linAccBias;
+            public Vector3 linVelSigma;
+            public Vector3 linVelBias;
         }
 
         private void Start()
         {
             this._trans = this.GetComponent<Transform>();
             this._rb = this.GetComponent<Rigidbody>();
-            this._geometryQuaternion = new Vector4();
             this._angularVelocity = new Vector3();
-            this._linearAcceleration = new Vector3();
+            this._linearVelocity = new Vector3();
         }
 
-        public void UpdateIMU()
+        public void UpdateDVL()
         {
             // Update Object State //
             // Calculate Move Element
             Vector3 localLinearVelocity = this._trans.InverseTransformDirection(this._rb.velocity);
-            Vector3 acceleration = (localLinearVelocity - this._lastVelocity) / Time.deltaTime;
             this._lastVelocity = localLinearVelocity;
-            // Add Gravity Element
-            acceleration += this._trans.InverseTransformDirection(Physics.gravity);
-
-            // Update //
 
             // Raw
-            this._geometryQuaternion = new Vector4(this._trans.rotation.x, this._trans.rotation.y, this._trans.rotation.z, this._trans.rotation.w);
             this._angularVelocity = -1 * this.transform.InverseTransformVector(this.GetComponent<Rigidbody>().angularVelocity);
-            this._linearAcceleration = acceleration;
+            this._linearVelocity = localLinearVelocity;
 
             // Apply Gaussian Noise
-             if (this.enableGaussianNoise) { this._geometryQuaternion = this.gaussianNoise.Apply(this._geometryQuaternion, this.setting.quatSigma); }
-             if (this.enableGaussianNoise) { this._angularVelocity = this.gaussianNoise.Apply(this._angularVelocity, this.setting.angVelSigma); }
-             if (this.enableGaussianNoise) { this._linearAcceleration = this.gaussianNoise.Apply(this._linearAcceleration, this.setting.linAccSigma); }
+            if (this.enableGaussianNoise) { this._angularVelocity = this.gaussianNoise.Apply(this._angularVelocity, this.setting.angVelSigma); }
+            if (this.enableGaussianNoise) { this._linearVelocity = this.gaussianNoise.Apply(this._linearVelocity, this.setting.linVelSigma); }
 
             // // Apply Bias Noise
-            // if (this.enableBiasNoise) { this._geometryQuaternion = this.biasNoise.Apply(this._geometryQuaternion, this.setting.quatSigma); }
             // if (this.enableBiasNoise) { this._angularVelocity = this.biasNoise.Apply(this._angularVelocity, this.setting.angVelSigma); }
             // if (this.enableBiasNoise) { this._linearAcceleration = this.biasNoise.Apply(this._linearAcceleration, this.setting.linAccSigma); }
         }
 
 #if UNITY_EDITOR
         [CustomEditor(typeof(IMU))]
-        public class IMUEditor : Editor
+        public class DVLEditor : Editor
         {
-            private IMU variables;
+            private DVL variables;
 
             private void Awake()
             {
-                this.variables = target as IMU;
+                this.variables = target as DVL;
             }
 
             // inspectorÇÃGUIê›íË
@@ -102,17 +89,15 @@ namespace FRJ.Sensor
                 if (this.variables.enableGaussianNoise)
                 {
                     EditorGUILayout.LabelField("Gaussian Noise Setting");
-                    this.variables.setting.quatSigma = EditorGUILayout.Vector4Field("->Quaternion Sigma", this.variables.setting.quatSigma);
                     this.variables.setting.angVelSigma = EditorGUILayout.Vector3Field("->AngularVelocity Sigma", this.variables.setting.angVelSigma);
-                    this.variables.setting.linAccSigma = EditorGUILayout.Vector3Field("->LinearAcceleration Sigma", this.variables.setting.linAccSigma);
+                    this.variables.setting.linVelSigma = EditorGUILayout.Vector3Field("->LinearAcceleration Sigma", this.variables.setting.linVelSigma);
                 }
                 this.variables.enableBiasNoise = EditorGUILayout.ToggleLeft("Enable Bias Noise", this.variables.enableBiasNoise);
                 if (this.variables.enableBiasNoise)
                 {
                     EditorGUILayout.LabelField("Bias Noise Setting");
-                    this.variables.setting.quatBias = EditorGUILayout.Vector4Field("->Quaternion Bias", this.variables.setting.quatBias);
                     this.variables.setting.angVelBias = EditorGUILayout.Vector3Field("->AngularVelocity Bias", this.variables.setting.angVelBias);
-                    this.variables.setting.linAccBias = EditorGUILayout.Vector3Field("->LinearAcceleration Bias", this.variables.setting.linAccBias);
+                    this.variables.setting.linVelBias = EditorGUILayout.Vector3Field("->LinearAcceleration Bias", this.variables.setting.linVelBias);
                 }
 
                 // GUIÇÃçXêVÇ™Ç†Ç¡ÇΩÇÁé¿çs
---
+++ Assets/UnitySensorsROS/Scripts/Runtime/DVL/DVLPublisher.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Robotics.ROSTCPConnector;
+using Unity.Robotics.ROSTCPConnector.ROSGeometry;
+using RosMessageTypes.Sensor;
+using RosMessageTypes.Geometry;
+
+[RequireComponent(typeof(FRJ.Sensor.DVL))]
+public class DVLPublisher : MonoBehaviour
+{
+
+    [SerializeField] private string _topicName = "dvl_twist";
+    [SerializeField] private string _frameId = "dvl";
+
+    private float _timeElapsed = 0f;
+    private float _timeStamp = 0f;
+
+    private ROSConnection _ros;
+    public TwistWithCovarianceStampedMsg _message;
+
+    private FRJ.Sensor.DVL _dvl;
+
+    void Start()
+    {
+        // Get Rotate Lidar
+        this._dvl = GetComponent<FRJ.Sensor.DVL>();
+
+        // setup ROS
+        this._ros = ROSConnection.instance;
+        this._ros.RegisterPublisher<ImuMsg>(this._topicName);
+
+        // setup ROS Message
+        this._message = new TwistWithCovarianceStampedMsg();
+        this._message.header.frame_id = this._frameId;
+    }
+
+    void Update()
+    {
+        this._timeElapsed += Time.deltaTime;
+
+        if (this._timeElapsed > (1f / this._dvl.scanRate))
+        {
+            // Update time
+            this._timeElapsed = 0;
+            this._timeStamp = Time.time;
+
+            // Update IMU data
+            this._dvl.UpdateDVL();
+
+            // Update ROS Message
+            int sec = (int)Math.Truncate(this._timeStamp);
+            uint nanosec = (uint)((this._timeStamp - sec) * 1e+9);
+            this._message.header.stamp.sec = sec;
+            this._message.header.stamp.nanosec = nanosec;
+            Vector3<FLU> angular_velocity_ros = new Vector3<FLU>(this._dvl.AngularVelocity).To<FLU>();
+            Vector3Msg angular_velocity =
+                new Vector3Msg(angular_velocity_ros.x,
+                               angular_velocity_ros.y,
+                               angular_velocity_ros.z);
+            this._message.twist.twist.angular = angular_velocity;
+            Vector3<FLU> linear_velocity_ros = new Vector3<FLU>(this._dvl.LinearVelocity).To<FLU>();
+            Vector3Msg linear_velocity =
+                new Vector3Msg(linear_velocity_ros.x,
+                               linear_velocity_ros.y,
+                               linear_velocity_ros.z);
+            this._message.twist.twist.linear = linear_velocity;
+
+            Vector3 angularVar = new Vector3(_dvl.setting.angVelSigma.x * _dvl.setting.angVelSigma.x, _dvl.setting.angVelSigma.y * _dvl.setting.angVelSigma.y, _dvl.setting.angVelSigma.z * _dvl.setting.angVelSigma.z);
+            Vector3 linearVar = new Vector3(_dvl.setting.linVelSigma.x * _dvl.setting.linVelSigma.x, _dvl.setting.linVelSigma.y * _dvl.setting.linVelSigma.y, _dvl.setting.linVelSigma.z * _dvl.setting.linVelSigma.z);
+            this._message.twist.covariance = new double[] {linearVar.x,0,          0,          0,            0,          0,
+                                                            0,          linearVar.y,0,          0,            0,          0,
+                                                            0,          0,          linearVar.z,0,            0,          0,
+                                                            0,          0,          0,          angularVar.x, 0,          0,
+                                                            0,          0,          0,          0,            angularVar.y,0,
+                                                            0,          0,          0,          0,            0,          angularVar.z};
+            this._ros.Send(this._topicName, this._message);
+        }
+    }
+}
