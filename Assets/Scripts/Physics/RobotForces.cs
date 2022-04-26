254a85e493b3d7ff340320fdabee9736662b8c37
Juched <prth.2000@gmail.com>
2022-04-26 17:55:01 -0400

    Added new robot forces file

--- Assets/Scripts/Physics/RobotForces.cs
+++ Assets/Scripts/Physics/RobotForces.cs
@@ -17,8 +17,8 @@ namespace physics
         public Vector3 robotCOM;
 
         private Rigidbody vehicle;
-
-        
+        public float depthBeforeSubmerged = .5f;
+        public float displacementAmt = 4f;
         private List<Thruster> thrusters;
         [SerializeField]
         private GameObject thrusterPrefab;
@@ -34,31 +34,34 @@ namespace physics
         private uint[] lastThrust;
         [SerializeField]
         private uint thrusterZeroValue;
-
+        private GameObject test;
         void Start()
         {
             thrusters = new List<Thruster>();
-            thrusterFab= new List<GameObject>();
-            if(thrusterPositions.Count != thrusterRotations.Count)
+            thrusterFab = new List<GameObject>();
+            if (thrusterPositions.Count != thrusterRotations.Count)
             {
                 Debug.LogError("Thruster positions don't equal amount of thruster rotations!");
             }
             vehicle = gameObject.GetComponent<Rigidbody>();
-            vehicle.centerOfMass = robotCOM;
+            vehicle.centerOfMass = robotCOM / 100 + solidWorksOrigin.localPosition;
+            test = Instantiate(thrusterPrefab, transform, false);
+            test.transform.localPosition = robotCOM / 100 + solidWorksOrigin.localPosition;
+
             lastThrust = new uint[thrusterPositions.Count];
-            for(int i = 0; i< thrusterPositions.Count; i++)
+            for (int i = 0; i < thrusterPositions.Count; i++)
             {
 
                 lastThrust[i] = thrusterZeroValue;
-                GameObject thr = Instantiate(thrusterPrefab, transform,false);
-                thr.transform.localPosition = (thrusterPositions[i] / 100)+solidWorksOrigin.localPosition;
+                GameObject thr = Instantiate(thrusterPrefab, transform, false);
+                thr.transform.localPosition = (thrusterPositions[i] / 100) + solidWorksOrigin.localPosition;
                 thr.transform.localRotation = Quaternion.Euler(thrusterRotations[i]);
 
                 thrusters.Add(thr.GetComponent<Thruster>());
                 thrusterFab.Add(thr.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject);
             }
 
-            
+
             //create ros connection
             ros = ROSConnection.GetOrCreateInstance();
             ros.Subscribe<PwmStampedMsg>(forcesTopic, recieveNewThrust);
@@ -69,15 +72,27 @@ namespace physics
             for (int i = 0; i < thrusters.Count; i++)
             {
                 float force = thrusters[i].ApplyForce((double)lastThrust[i], i);
-                if(force > 0){
+                if (force > 0)
+                {
                     thrusterFab[i].GetComponent<Renderer>().material.color = Color.red;
                 }
             }
+            // print(test.transform.position);
+
+            // if (this.vehicle.transform.position.y < -0.1f)
+            // {
+            //     float displacementMultiplier = Mathf.Clamp01(-this.vehicle.transform.position.y / depthBeforeSubmerged) * displacementAmt;
+            //     //print(displacementMultiplier);
+            //     this.vehicle.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
+            // }
+
         }
 
-        void recieveNewThrust(PwmStampedMsg msg) {   
-               
-            for(int i=0; i<msg.pwm.Length; i++) {
+        void recieveNewThrust(PwmStampedMsg msg)
+        {
+
+            for (int i = 0; i < msg.pwm.Length; i++)
+            {
                 //print(i+" | "+msg.pwm[i]); 
                 lastThrust[i] = msg.pwm[i];
             }
