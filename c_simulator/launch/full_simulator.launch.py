1c24eb36397f13963e5b446e0f1748fded86a8e7
Brach Knutson <brach2003@gmail.com>
2024-08-03 05:49:34 -0400

    make camera faker optional

--- c_simulator/launch/full_simulator.launch.py
+++ c_simulator/launch/full_simulator.launch.py
@@ -2,7 +2,7 @@ import launch
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import AnyLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration as LC
 import os
 
@@ -10,12 +10,32 @@ physics_launch = os.path.join(get_package_share_directory('c_simulator'), 'launc
 camera_launch = os.path.join(get_package_share_directory('camera_faker'), 'launch', 'zedfaker.launch.py')
 rviz_launch = os.path.join(get_package_share_directory('riptide_rviz'), 'launch', 'rviz_start.launch.py')
 
+
+def launch_camera_faker(context, **kwargs):
+    with_faker = LC("with_camera_faker").perform(context)
+    if with_faker == "True":
+        return [
+            IncludeLaunchDescription(
+                AnyLaunchDescriptionSource(camera_launch),
+                launch_arguments=[
+                    ('robot', LC('robot'))
+                ]
+            )
+        ]
+
+
 def generate_launch_description():
     return launch.LaunchDescription([
         DeclareLaunchArgument(
             "robot",
             default_value="talos",
             description="Name of the vehicle",
+        ),
+        
+        DeclareLaunchArgument(
+            "with_camera_faker",
+            default_value="True",
+            description="Enable or disable launch of the camera faker."
         ),
         
         #physics simulator
@@ -27,12 +47,7 @@ def generate_launch_description():
         ),
         
         #camera faker
-        IncludeLaunchDescription(
-            AnyLaunchDescriptionSource(camera_launch),
-            launch_arguments=[
-                ('robot', LC('robot'))
-            ]
-        ),
+        OpaqueFunction(function = launch_camera_faker),
         
         #rviz
         IncludeLaunchDescription(
